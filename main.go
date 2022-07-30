30c1488be0c15c10f7ede3213996f4683d1ed511
taylormeador <taylor.r.meador@gmail.com>
2022-07-30 09:24:30 -0600

    test get request

--- main.go
+++ main.go
@@ -15,12 +15,34 @@ func initAll() {
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Fprintf(w, "Welcome to the home page")
 	fmt.Println("Endpoint Hit: homePage")
+}
+
+func isLegal(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This endpoint parses the FEN string and returns the updated game state")
+	fmt.Println("Endpoint Hit: isLegal")
+
+	// testing getting GET parameters
+	keys, ok := r.URL.Query()["key"]
+
+	if !ok || len(keys[0]) < 1 {
+		log.Println("Url Param 'key' is missing")
+		return
+	}
+
+	// Query()["key"] will return an array of items,
+	// we only want the single item.
+	key := keys[0]
+
+	log.Println("Url Param 'key' is: " + string(key))
+
+	//json.NewEncoder(w).Encode(FENstring)
 }
 
 func handleRequests(port string) {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/isLegal", isLegal)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 

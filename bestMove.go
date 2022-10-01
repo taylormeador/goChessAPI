01875ca482cc926344f6b319d37fcce19dad4ae2
taylormeador <taylor.r.meador@gmail.com>
2022-10-01 15:36:41 -0600

    remove book move checking in best move search

--- bestMove.go
+++ bestMove.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 )
 
 var DEPTH = 6
 
 func findBestMove(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This endpoint parses the FEN string and returns the best move the engine can find")
+	fmt.Fprintf(w, "This endpoint parses the FEN string and returns the best move")
 	fmt.Println("Endpoint Hit: bestMove")
 
 	// testing getting GET parameters
@@ -37,13 +35,7 @@ func findBestMove(w http.ResponseWriter, r *http.Request) {
 	log.Printf("parsePosition(formattedFEN): %t", isFENLegal)
 
 	// find best move
-	bookMove := checkForBookMove(FEN)
-	if bookMove != "No book move found" {
-		log.Println("Found book move") // debug
-		FEN = bookMove
-	} else {
-		bestMove = searchPosition(DEPTH)
-	}
+	bestMove = searchPosition(DEPTH)
 
 	// build json struct
 	response := FENjson{FEN: FEN}
@@ -59,34 +51,34 @@ func findBestMove(w http.ResponseWriter, r *http.Request) {
 
 }
 
-type bookMove struct {
-	FEN string
-}
+//type bookMove struct {
+//	FEN string
+//}
 
 // look at the opening_book db table and see if we can just return the best move from there instead of searching/evaluating manually
-func checkForBookMove(FEN string) string {
-	// Create DB pool
-	DatabaseURL := os.Getenv("DATABASE_URL")
-	db, err := sql.Open("postgres", DatabaseURL)
-	if err != nil {
-		log.Fatal("Failed to open a DB connection: ", err)
-	}
-	defer db.Close()
-
-	// Create an empty best move struct and make the sql query (using $1 for the parameter)
-	var topBookMove bookMove
-	query := "SELECT move_1 FROM opening_book WHERE fen = $1"
-	err = db.QueryRow(query, FEN).Scan(&topBookMove.FEN)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			log.Printf("No book move found for FEN: %s", FEN) // debug
-			return "No book move found"
-		} else {
-			log.Fatal("Failed to execute query: ", err)
-		}
-	}
-
-	// return FEN with the best move made on the board
-	fmt.Printf("Book move result: %s\n", topBookMove.FEN)
-	return topBookMove.FEN
-}
+//func checkForBookMove(FEN string) string {
+//	// Create DB pool
+//	DatabaseURL := os.Getenv("DATABASE_URL")
+//	db, err := sql.Open("postgres", DatabaseURL)
+//	if err != nil {
+//		log.Fatal("Failed to open a DB connection: ", err)
+//	}
+//	defer db.Close()
+//
+//	// Create an empty best move struct and make the sql query (using $1 for the parameter)
+//	var topBookMove bookMove
+//	query := "SELECT move_1 FROM opening_book WHERE fen = $1"
+//	err = db.QueryRow(query, FEN).Scan(&topBookMove.FEN)
+//	if err != nil {
+//		if err == sql.ErrNoRows {
+//			log.Printf("No book move found for FEN: %s", FEN) // debug
+//			return "No book move found"
+//		} else {
+//			log.Fatal("Failed to execute query: ", err)
+//		}
+//	}
+//
+//	// return FEN with the best move made on the board
+//	fmt.Printf("Book move result: %s\n", topBookMove.FEN)
+//	return topBookMove.FEN
+//}
